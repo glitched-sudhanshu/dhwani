2f7aa2b3ff4ce3b108afc7144c21eda64f7950cd
sudhanshu <r02.sudhanshu@gmail.com>
2022-07-27 09:44:03 +0530

    App finished

--- app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/MusicNotificationManager.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/MusicNotificationManager.kt
@@ -57,6 +57,7 @@ class MusicNotificationManager(
     ) : PlayerNotificationManager.MediaDescriptionAdapter {
 
         override fun getCurrentContentTitle(player: Player): CharSequence {
+            newSongCallback()
             return mediaController.metadata.description.title.toString()
         }
 
